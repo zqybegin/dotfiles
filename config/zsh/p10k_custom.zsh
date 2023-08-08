4eeb7a3e70f11ffc5aa6127c2a84181ec0d2c68d
22040953-Zhang Qingyu <zqybegin@gmail.com>
2023-08-08 09:35:31 +0800

    p10k: change prompt symbol

--- config/zsh/p10k_custom.zsh
+++ config/zsh/p10k_custom.zsh
@@ -2,3 +2,11 @@
 typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=default
 typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
 typeset -g POWERLEVEL9K_SHORTEN_DELIMITER='..'
+
+# Default current directory color.
+typeset -g POWERLEVEL9K_DIR_FOREGROUND=117
+
+# prompt symbol if the last command succeeded.
+# typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=189
+
+
